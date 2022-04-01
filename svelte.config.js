552fc9043b31420eda9e0de00b04cef4d13de5f8
William Mckee <99625972+williammckee-techtorium@users.noreply.github.com>
2022-04-02 08:26:57 +1300

    add netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import WindiCSS from 'vite-plugin-windicss'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,12 +6,7 @@ const config = {
 	kit: {
         // hydrate the <div id="svelte"> element in src/app.html
 
-        adapter: adapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        }),
+        adapter: adapter(),
 		vite: {
 			plugins: [
 			    WindiCSS(),
